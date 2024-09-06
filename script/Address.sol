6f76f6e19b77f9dfe3e698cd5188570c12b5e0e0
UrsaMajor-t <changsheng010909@gmail.com>
2024-09-06 11:54:51 +0800

    update contract deploy address

--- script/Address.sol
+++ script/Address.sol
@@ -5,25 +5,25 @@ contract AddressHelper {
     uint32 sepoliaEid = 40161;
     uint32 arbitrumSepoliaEid = 40231;
 
-    address sepoliaSyncRouter = 0x1996a9faAE7b27D2cb36eF36A912b086F83b2d56;
+    address sepoliaSyncRouter = 0x453C825cA5B8EdD85D531c3A3A01EE488670c2a7;
     address arbitrumSepoliaSyncRouter =
-        0x84E04a0f1DE51C6DcC145d3C3463B62B4cBE8152;
+        0x453C825cA5B8EdD85D531c3A3A01EE488670c2a7;
 
-    address sepoliaEntryPoint = 0xA5e3E0E175Cf4c5E55527DcEB043e8DDe07c4E35;
+    address sepoliaEntryPoint = 0x5F2464f924b7D9166a870cCe9201AFBC2a2f151D;
     address arbitrumSepoliaEntryPoint =
-        0x5f8AB9b43f0dd3661DD14d9F17787A7d12434d9D;
+        0x5F2464f924b7D9166a870cCe9201AFBC2a2f151D;
 
-    address sepoliaFactory = 0x48682E02E249B7FF3f4c7BC41B14F6A425602138;
-    address arbitrumSepoliaFactory = 0x38BFE564a87531DD4a18b4Db222FDd3bDb3feF53;
+    address sepoliaFactory = 0x19B8495e7D3C0Ff16592f67745a0c887D3d60a4D;
+    address arbitrumSepoliaFactory = 0x19B8495e7D3C0Ff16592f67745a0c887D3d60a4D;
 
-    address verifier = 0x21C81909C1C8F2D1EDC67730B1967A2079b0D164;
+    address verifier = 0xBCf327CD63FA62D25A5c261eCb05ee67Dc96fc2D;
 
     uint32[] sepoliaDstEids = [arbitrumSepoliaEid];
 
     address public sp1Verifier = 0x3B6041173B80E77f038f3F2C0f9744f04837185e;
     bytes32 public aaProgramVKey =
-        0x00562de31787f72cba4fffa5794e5500c92f40c0ac88e0cbb3b732460b096908;
+        0x0027af29febf490e181e0586f4d6967e560e9c0c9b8e9c4b8959a8cb136ad980;
 
     address endPoint = 0x6EDCE65403992e310A62460808c4b910D972f10f;
-    address owner = 0xBC989fDe9e54cAd2aB4392Af6dF60f04873A033A;
+    address owner = 0xeac2F980834c3a27CFDD52a3073468A8aA07c12C;
 }
